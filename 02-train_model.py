c2d0386a5da83985430a90614256e745d7e7eb33
omkar-vitb <omkar.25bai10893@vitbhopal.ac.in>
2026-03-27 18:43:50 +0530

    Update 02-train_model.py

--- 02-train_model.py
+++ 02-train_model.py
@@ -13,7 +13,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-os.makedirs("outputs",exist_ok=True)
+
 os.makedirs("models",exist_ok=True)
 
 print("="*55)
